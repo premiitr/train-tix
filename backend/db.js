26cdc429fe50033c8fb217d7dfb54edce32c5333
Prem kumar <pkumar2@ph.iitr.ac.in>
2025-04-12 09:39:53 +0530

    backend_deployed on server

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,14 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432, // Render uses 5432 by default
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
